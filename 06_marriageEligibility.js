873a8fddc0c8b1d85ab3afa7349d5cd1518b5216
Prakash5288 <153574745+Prakash5288@users.noreply.github.com>
2024-01-04 22:22:06 +0530

    January 4 updates

---
+++ 06_marriageEligibility.js
@@ -0,0 +1,23 @@
+function merriageEligibility(gender, age) {
+  if (gender.includes("female") && age >= 18) {
+    console.log(`Gender= ${gender} age=${age} she is Eligible for Merriage Bcoz her age is ${age}`);
+  } else if (gender.includes("female") && age < 18) {
+    console.log(
+      `Gender= ${gender} age=${age} unfortunately she is Not Eligible for Merriage Bcoz her age is ${age}`
+    );
+  } else if (gender.includes("male") && age >= 21) {
+    console.log(`Gender= ${gender} age=${age} He is Eligible for Merriage Bcoz his age is ${age}`);
+  } else if (gender.includes("male") && age < 21) {
+    console.log(
+      `Gender= ${gender} age=${age} unfortunately He is Not Eligible for Merriage Bcoz his age is ${age}`
+    );
+  } else {
+    console.log("You have entered  wrong data....!");
+  }
+}
+merriageEligibility("male", 17);
+merriageEligibility("male", 25);
+merriageEligibility("female", 28);
+merriageEligibility("female", 16);
+merriageEligibility("other", 35);
+merriageEligibility("other", 41);
---
+++ 06_switchCaseMonth.js
@@ -0,0 +1,59 @@
+function switchCasemonth(month){
+    switch (month) {
+        case 1:
+            console.log("Month 01 = January");
+            break;
+        case 2:
+            console.log("Month 01 = January");
+            break;
+            case 2:
+            console.log("Month 02 = February");
+            break;
+            case 3:
+            console.log("Month 03 = March");
+            break;
+            case 4:
+            console.log("Month 04 = April");
+            break;
+            case 5:
+            console.log("Month 05 = May");
+            break;
+            case 6:
+            console.log("Month 06 = June");
+            break;
+            case 7:
+            console.log("Month 07 = July");
+            break;
+            case 8:
+            console.log("Month 08 = Agust");
+            break;
+            case 9:
+            console.log("Month 09 = September");
+            break;
+            case 10:
+            console.log("Month 10 = Octomber");
+            break;
+            case 11:
+            console.log("Month 11 = November");
+            break;
+            case 12:
+            console.log("Month 12 = December");
+            break;
+        default: console.log(`Invalid Month ${month}`)
+            break;
+    }
+}
+switchCasemonth(1);
+switchCasemonth(2);
+switchCasemonth(3);
+switchCasemonth(4);
+switchCasemonth(5);
+switchCasemonth(6);
+switchCasemonth(7);
+switchCasemonth(8);
+switchCasemonth(9);
+switchCasemonth(10);
+switchCasemonth(11);
+switchCasemonth(12);
+switchCasemonth(13);
+switchCasemonth(14);
